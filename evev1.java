d8b3899adf3f38821872773e39cef9cebdfce356
Elakiyaaa <elakthamizh@gmail.com>
2017-06-05 14:59:05 +0530

    Add files via upload

---
+++ evev1.java
@@ -0,0 +1,19 @@
+package even1;
+
+import java.util.Scanner;
+
+public class evev1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("ENTER THE NUMBER  ");
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		if(a%2==0)
+			System.out.print("EVEN NUMBER");
+		else
+			System.out.println("ODD NUMBER");
+
+	}
+
+}
